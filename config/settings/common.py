145479d140fb7287b5ba2cc12b448747f95602a0
kyungjunleeme <kyungjunlee.me@gmail.com>
2021-09-24 18:51:52 +0900

    deploy error

--- config/settings/common.py
+++ config/settings/common.py
@@ -149,7 +149,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
+
+STATIC_URL = '/static/'
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
+STATICFILES_DIRS = [BASE_DIR / "baemin_web" / "static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,13 @@
 from .common import *
+DEBUG=False
+#ALLOWED_HOSTS = ["146.56.135.69"]
+#ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["*"]
 
-ALLOWED_HOSTS = ["146.56.135.69"]
+
+#/home/ubuntu/baemin_st_web/.venv/mysite.env
+
+#/home/ubuntu/baemin_st_web/.venv/bin/gunicorn
+
+# /home/ubuntu/baemin_st_web/staticfiles
+# 146.56.135.69
