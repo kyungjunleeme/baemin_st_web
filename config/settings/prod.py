a9698b496acfc75919bd158c0f529537071710e9
kyungjunleeme <kyungjunlee.me@gmail.com>
2021-09-25 16:02:02 +0900

    apply SSL to nginx

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,9 +1,11 @@
 from .common import *
 
-DEBUG = False
-ALLOWED_HOSTS = ["146.56.135.69"]
-# ALLOWED_HOSTS = ["localhost"]
-# ALLOWED_HOSTS = ["*"]
+DEBUG = True
+ALLOWED_HOSTS = ["146.56.135.69","baeminweb.co.kr", "www.baeminweb.co.kr"]
+#ALLOWED_HOSTS = ["*"]
+
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
+#STATICFILES_DIRS = []
 
 
 # /home/ubuntu/baemin_st_web/.venv/mysite.env
